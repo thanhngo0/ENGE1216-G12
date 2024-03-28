5057288f05d7188fc81eab60831c84667691ee67
spatialsun17 <127224328+spatialsun17@users.noreply.github.com>
2024-03-28 10:36:58 -0400

    Update dimensionsofbuild.py

--- dimensionsofbuild.py
+++ dimensionsofbuild.py
@@ -1,2 +1,12 @@
-def dimensionsofbuild():
-    inp
+def main():
+    which = input("What are you looking for?")
+                    "1: dimensions of the ladder extension"
+                    "2: space available for storage" )
+    dimensionsofshape()
+    storagespace()
+
+
+def dimensionsofshape()
+    #determines the shape of the build
+def storagespace()
+    
