729f68ab830d9353e9c4d0cd259c1ffbb5febb25
spatialsun17 <127224328+spatialsun17@users.noreply.github.com>
2024-03-28 10:47:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def main():
-    which = input("What are you looking for?")
-                    "1: dimensions of the ladder extension"
-                    "2: space available for storage" )
-    dimensionsofshape()
-    storagespace()
+    which = input("What are you looking for? 1: dimensions of the ladder extension 2: space available for storage 3: Both" )
+    if which == 1: 
+        dimensionsofshape()
+    elif which == 2:
+        storagespace()
+    elif which == 3:
+        dimensionsofshape()
+        storagespace()
+    else:
+        "Please input 1, 2, or 3"
 
 
 def dimensionsofshape()
