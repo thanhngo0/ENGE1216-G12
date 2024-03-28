3945beee68621679b046ae2322b9a0dd576d0223
spatialsun17 <127224328+spatialsun17@users.noreply.github.com>
2024-03-28 16:05:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,30 @@ def main():
 
 
 def dimensionsofshape():{
-    print(1)
+    
     #determines the shape of the build
     }
 
 def storagespace():{
-    print(2)
-    #determine how much storage is available
+    basewidth = int(input("How wide is the base of the ladder storage area? (in inches)"))
+    baselength = int(input("How long is the base of the ladder storage area? (in inches)"))
+    height = int(input("How tall are the sides of the ladder storage area? (in inches)"))
+
+    volumeofstorage = basewidth * baselength * height
+
+    insert = str(input("Is there an insert that will be placed inside of the storage area? (yes or no)"))
+
+    if insert == "yes" or "y" or "yeah":
+        insertthickness = int(input("What is the thickness of the insert? (in inches)"))
+        insertheight = int(input("How many inches up the side of the storage area will the insert be placed?"))
+        
+        volumeabove insert = baselength * basewidth * (height - (thickness + insertheight))
+        volumebelow = baselength * basewidth * (insertheight)
+
+        print ("The available space above the insert is: " + str(volumeabove) + " in^3" )
+        print ("The available space below the insert is: " + str(volumebelow) + " in^3" )
+
+    elif insert == "no" or "n" or "nah":
+        print ("The available space in the ladder extension is: " + str(volumeofstorage) + " in^3" )
 }
 main()
